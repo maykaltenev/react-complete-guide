4219c2ad41057ec313a2c4c689e0cd19a7f82971
maykaltenev <michaeltenev@gmail.com>
2022-06-07 22:20:16 +0200

    splitting components

--- section-03/react-components/src/App.js
+++ section-03/react-components/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 // custom name components is named with capital letters
 function App(props) {
   const expenses = [
@@ -22,25 +22,12 @@ function App(props) {
       date: new Date(2021, 5, 12),
     },
   ];
-
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>This is also visible</p>
-      <ExpenseItem title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date} />
-      <ExpenseItem title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date} />
-      <ExpenseItem title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date} />
-      <ExpenseItem title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date} />
-    </div>
+      <Expenses items={expenses} />
+    </div >
   );
 }
-
 export default App;
