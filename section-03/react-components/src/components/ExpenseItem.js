1b72e9d4f173312aa9e6ca9c8aa31a2068d6910e
maykaltenev <michaeltenev@gmail.com>
2022-06-07 21:45:38 +0200

    components

--- section-03/react-components/src/components/ExpenseItem.js
+++ section-03/react-components/src/components/ExpenseItem.js
@@ -1,16 +1,14 @@
 // only one root element per component
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expanseAmount = '294.67'
+function ExpenseItem(props) {
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toISOString().slice(0, 10)}</div>
+            <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>{expanseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount.toFixed(2)}</div>
             </div>
         </div>
     )
