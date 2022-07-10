78b43b3153b1c2a6dd64da4304772e7be452d87e
maykaltenev <michaeltenev@gmail.com>
2022-07-10 23:38:50 +0200

    adding users, handeling errors and styling

--- section-08/src/App.js
+++ section-08/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((prevState) => {
+      return [...prevState, { name: userName, age: userAge }]
+    })
+  }
   return (
     <div>
-
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={usersList} />
     </div>
   );
 }
---
+++ section-08/src/components/Users/UserList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from '../UI/Card'
+
+import classes from './UserList.module.css'
+
+const UserList = (props) => {
+    return (
+        <Card className={classes.users}>
+            <ul>{props.users.map((user, i) => (
+                <li key={i}>
+                    {user.name} ({user.age} years old)
+                </li>
+            ))}
+            </ul>
+        </Card>
+    );
+};
+export default UserList
