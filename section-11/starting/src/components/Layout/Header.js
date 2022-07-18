43b0e7f7137daf54b04f06ec1d1a86c636930570
maykaltenev <michaeltenev@gmail.com>
2022-07-18 23:17:59 +0200

    react meals

---
+++ section-11/starting/src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import { Fragment } from 'react'
+
+import meals from "../../assets/meals.jpg"
+import classes from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton'
+export default function Header() {
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1>React Meals</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={classes['main-image']}>
+                <img src={meals} alt='A table full of delicious food' />
+            </div>
+        </Fragment>
+    )
+}
