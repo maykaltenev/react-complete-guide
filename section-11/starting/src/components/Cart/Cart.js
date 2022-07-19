258be37e7da6a5e1cb1f030346e8f5d954379e78
maykaltenev <michaeltenev@gmail.com>
2022-07-19 09:18:35 +0200

    cart

--- section-11/starting/src/components/Cart/Cart.js
+++ section-11/starting/src/components/Cart/Cart.js
@@ -2,18 +2,17 @@ import Modal from '../UI/Modal'
 import React from 'react'
 import classes from './Cart.module.css'
 export default function Cart(props) {
-    const cartItems = <ul className={classes["cart - items"]}> {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map(((item, i) => <li key={i}>{item.name}</li>))}</ul >
+    const cartItems = <ul className={classes['cart-items']}> {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map(((item, i) => <li key={i}>{item.name}</li>))}</ul >
     return (
         <Modal>
-            {cartItems}
             <div>
-                cart items
+                {cartItems}
                 <div className={classes.total}>
                     <span>Total Amount</span>
                     <span>35.62</span>
                 </div>
                 <div className={classes.actions}>
-                    <button className={classes['button--alt']}>Close</button>
+                    <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
                     <button className={classes.button}>Order</button>
                 </div>
             </div>
--- section-11/starting/src/components/Layout/Header.js
+++ section-11/starting/src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import { Fragment } from 'react'
 import meals from "../../assets/meals.jpg"
 import classes from './Header.module.css'
 import HeaderCartButton from './HeaderCartButton'
-export default function Header() {
+export default function Header(props) {
     return (
         <Fragment>
             <header className={classes.header}>
                 <h1>React Meals</h1>
-                <HeaderCartButton />
+                <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className={classes['main-image']}>
                 <img src={meals} alt='A table full of delicious food' />
