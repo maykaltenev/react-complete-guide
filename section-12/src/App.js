2d242b4f4c37721f110715c6901d33c6fdc8d7a3
maykaltenev <michaeltenev@gmail.com>
2022-07-20 19:01:26 +0200

    starting

--- section-12/src/App.js
+++ section-12/src/App.js
@@ -1,8 +1,17 @@
+import React, { useState } from 'react';
+import './App.css';
+import Button from './components/UI/Button';
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const toggleParagraphHandle = () => {
+    setShowParagraph(prevShowParagraph => !prevShowParagraph)
+  }
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <div className="app">
+      <h1>Hi there!</h1>
+      {showParagraph && <p>This is now!</p>}
+      <Button onClick={toggleParagraphHandle}>Toggle Paragraph</Button>
+    </div >
   );
 }
 
