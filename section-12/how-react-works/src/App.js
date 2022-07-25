f6070d950c06ffc277d56b6aac2532d5b09bc93f
maykaltenev <michaeltenev@gmail.com>
2022-07-25 12:19:59 +0200

    state schedule

--- section-12/how-react-works/src/App.js
+++ section-12/how-react-works/src/App.js
@@ -1,18 +1,28 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import Button from "./components/UI/Button/Button"
 import './App.css';
 import DemoOutput from './Demo/DemoOutput';
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(true)
-  const toggleParagraph = () => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph)
+  const [showParagraph, setShowParagraph] = useState(false)
+  const [allowToggle, setAllowToggle] = useState(false)
+  const toggleParagraph = useCallback(() => {
+    if (allowToggle) {
+      // This will ensure that react will always
+      // use the latest state (not looking on the last time state was rendered,
+      //  but the state the scheduled state), because React is postponing rendering and preoretasing state update
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle])
+  const allowToggleHandler = () => {
+    setAllowToggle(true)
   }
   return (
     <div className="app">
       <h1>Hi there!</h1>
       {showParagraph && <p>This is new!</p>}
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
       <Button onClick={toggleParagraph}>Show Paragraph</Button>
     </div>
   );
