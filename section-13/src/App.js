fe83c2d58d10158d1222a524b349ea3ecb8f21d0
maykaltenev <michaeltenev@gmail.com>
2022-07-28 13:45:59 +0200

    user-context

--- section-13/src/App.js
+++ section-13/src/App.js
@@ -12,20 +12,29 @@
 
 
 import UserFinder from './components/UserFinder';
-// import UsersContext from './store/users-context';
+import UsersContext from './store/users-context';
 
-
+export const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+  { id: 'u4', name: 'Nico' },
+  { id: 'u5', name: 'Rico' },
+  { id: 'u6', name: 'Kate' },
+  { id: 'u7', name: 'Horry' },
+  { id: 'u8', name: 'May' },
+  { id: 'u9', name: 'John' },
+];
 function App() {
-  // const usersContext = {
-  //   users: DUMMY_USERS
-  // }
+  const usersContext = {
+    users: DUMMY_USERS
+  }
 
   return (
-    //  <UsersContext.Provider value={usersContext}>
-    <div>
+    <UsersContext.Provider value={usersContext}>
       <UserFinder />
-      {/* // </UsersContext.Provider> */}
-    </div >
+    </UsersContext.Provider>
+
   );
 }
 
